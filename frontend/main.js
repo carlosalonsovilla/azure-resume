f7eabecf58ed6bed5b7bb2152564172f1620aec8
carlosalonsovilla <cav2589@gmail.com>
2024-09-28 20:36:30 -0400

    Added environment variables to the frontend main.js.

--- frontend/main.js
+++ frontend/main.js
@@ -1,23 +1,18 @@
-const functionUrl = ""; // Replace with your actual function URL
+import config from './config.js';
 
 async function updateCounter() {
-    try {
-        const response = await fetch(functionUrl);
-        
-        if (response.ok) {
-            const data = await response.json(); // Parse the JSON response
-            
-            const counterValue = data.count;
-
-            // Update the HTML element with id "counter"
-            document.getElementById('counter').textContent = counterValue;
-        } else {
-            console.error('Failed to fetch counter:', response.status);
-        }
-    } catch (error) {
-        console.error('Error:', error);
+  try {
+    const response = await fetch(config.functionUrl);
+    if (response.ok) {
+      const data = await response.json();
+      const counterValue = data.count;
+      document.getElementById('counter').textContent = counterValue;
+    } else {
+      console.error('Failed to fetch counter:', response.status);
     }
+  } catch (error) {
+    console.error('Error:', error);
+  }
 }
-
 
 window.onload = updateCounter;
