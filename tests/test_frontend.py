981e754fe3396cc4a0f4aa8fa1cb1f2eb6d300fa
carlosalonsovilla <cav2589@gmail.com>
2024-09-29 22:47:31 -0400

    Updated test_frontend.py to include additional tests.

--- tests/test_frontend.py
+++ tests/test_frontend.py
@@ -4,17 +4,35 @@ import time
 
 driver = webdriver.Chrome()
 
-driver.get("http://localhost:5500/frontend/index.html")
+driver.get("https://www.carlosalonsovilla.com")
 
 time.sleep(3)
 
+# Test 1: Check title
 title = driver.title
 assert title == "Carlos Villa's Resume", f"Title does not match. Found: {title}"
 print("Title matches successfully.")
 
+# Test 2: Check if the counter exists
 counter_element = driver.find_element(By.ID, "counter")
 assert counter_element is not None, "Counter element not found."
 print("Counter element found successfully.")
+
+# Test 3: Check if Linkedin link is present and correct
+linkedin = driver.find_element(By.CSS_SELECTOR, "a[href='https://www.linkedin.com/in/carlos-villa-63236927b/']")
+assert linkedin is not None, "Linkedin link not found."
+print("Linkedin list found successfully.")
+
+# Test 4: Check if Github link is present and correct
+github = driver.find_element(By.CSS_SELECTOR, "a[href='https://github.com/carlosalonsovilla/azure-resume']")
+assert github is not None, "Github link not found."
+print("Github link found successfully.")
+
+# Test 5: Check if profile image is present
+profile_pic = driver.find_element(By.CSS_SELECTOR, "img[src='images/me.png']")
+assert profile_pic is not None, "Profile Picture was not found."
+print("Profile image found successfully")
+
 
 print("Test passed successfully!")
 
